adf3c0455e5d6361b856a38853a8e17c8912bf54
Badr-Skalli <test.hps.email@gmail.com>
2026-09-21 15:53:50 -0400

    Saving Task 7: Write the TotalCost component

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -3,24 +3,24 @@ import "./TotalCost.css";
 
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
   
+    const total_amount = totalCosts.vanue + totalCosts.av + totalCosts.meals;
 
-  return (
-    <div className="pricing-app">
-      <div className="display_box">
-        <div className="header">
-          <p className="preheading"><h3>Total cost for the event</h3></p>
-        </div>
-        <div>
-          <h2 id="pre_fee_cost_display" className="price">
-           
-          </h2>
-         
+    return (
+        <div className="pricing-app">
+        <div className="display_box">
+            <div className="header">
+                <p className="preheading"><h3>Total cost for the event</h3></p>
+            </div>
             <div>
-             
+                <h2 id="pre_fee_cost_display" className="price">
+                    ${total_amount}
+                </h2>            
+                <div className='renderItems'>
+                    <ItemsDisplay />
+                </div>
             </div>
         </div>
-      </div>
-    </div>
+        </div>
   );
 };
 
